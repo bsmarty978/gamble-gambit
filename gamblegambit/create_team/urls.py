8e48af99f3c0583fe0293427efd0512cd84d6752
Bhavesh Chavda <Bsmarty@outlook.com>
2021-01-16 21:17:05 +0530

    Added: Captain select method | blue

--- gamblegambit/create_team/urls.py
+++ gamblegambit/create_team/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns =[
     path('<str:title>/',views.create_team,name="create_team"),
+    # path('upcoming',views.sample,name="sample"),   #can be used when creating upcmoing match model  inside create matches folder
     path('sample',views.sample,name="sample"),
 ]
